ca8d4f34e3a84292de9e7b12b9c81bc9a021e89e
Khoa Huynh <huynhduykhoa03@gmail.com>
2022-06-18 23:46:52 +0700

    chapter 2 video 6

---
+++ javascript-basic-video3/video3/video3.js
@@ -0,0 +1,45 @@
+
+//let x = 5, y = 6, z = 7;
+
+
+
+
+//console.log(`hello '"' ' '"" '" world from javascript`, "x =", x, "y =", y, "z =", z)
+
+let object = [{
+    ten: 'Huynh Duy Khoa', 
+    tuoi: '20',
+},
+{
+    ten: 'Tuong Vinh', 
+    tuoi: '30',
+}]
+
+//lay obj 2
+
+console.log(object) 
+
+
+
+// let a = 5; 
+// //a = 15 
+
+// let b = 20;
+// a = b
+
+// const c = 20;
+// c = a
+
+// console.log(c)
+
+
+
+
+console.log('I\'m Khoa')
+
+console.log('Tôi tên là', object[1].ten, ',Tuổi:', object[1].tuoi)
+
+console.log(`Tôi tên là ${object[1].ten}`) 
+
+let text = "        hello world!          ";
+let newText = text.replace("world", "Khoa");
---
+++ javascript-basic-video3/video6/video6.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5
+let b = 5.1
+let c = 6.9
+let d = '5'
+let e = +d;
+let f = Number(d)
+console.log('e value:', e, 'type:', typeof e)
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d)
