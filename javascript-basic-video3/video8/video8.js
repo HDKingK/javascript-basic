f505709a21e19501da2c5109dbb93d34161aa96a
Khoa Huynh <huynhduykhoa03@gmail.com>
2022-06-19 17:08:27 +0700

    chapter 2 video 8

---
+++ javascript-basic-video3/video8/video8.js
@@ -0,0 +1,16 @@
+console.log('hello world from HTML')
+
+//key: value
+let a = {
+    name: 'Khoa',
+    address: 'Quan 8',
+    email: '',
+    gender: ''
+};
+
+let b = 'Khoa';
+
+a.name = 'Vinh'
+
+console.log('what is your name ?', 'My name is:', a.name, a)
+console.log('where are you from ?', 'I am from:', a['address'])
