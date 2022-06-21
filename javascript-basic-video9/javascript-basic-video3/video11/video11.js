a70c8e6733c54974070a14d3fb6a53baa5e521bf
Khoa Huynh <huynhduykhoa03@gmail.com>
2022-06-21 10:29:50 +0700

    chapter 3 video 12

--- javascript-basic-video9/javascript-basic-video3/video11/video11.js
+++ javascript-basic-video9/javascript-basic-video3/video11/video11.js
@@ -3,13 +3,13 @@ console.log('Hello world from HTML')
 
 let arr = ['Mu', 'MC', 'Liv', 'Chelsea']
 
-let i = 0
+//let i = 0
 
-for(let i = 0; i < arr.length; ++i){
+for(let i = 0; i < arr.length; i++){
     console.log('check variable i = ', arr[i])
 }
 
-console.log('check value i: ', i)
+//console.log('check value i: ', i)
 
 // i++ === se bang voi i = i + 1
 // ++i
---
+++ javascript-basic-video9/javascript-basic-video3/video12/video12.js
@@ -0,0 +1,27 @@
+console.log('Hello world from HTML');
+
+let arr = [`MC`, `Liv`, `Mu`, `Chelsea`];
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(`Top: `, i + 1, arr[i]);
+// };
+
+let i = 0;
+// while(i < arr.length){
+//     console.log(`Top: `, i + 1, arr[i]);
+//     i++;
+// };
+
+do {
+    console.log(`Top: `, i + 1, arr[i]);
+    i++;
+} while(i < 4);
+
+
+// let flag = false;
+
+// while(!flag){
+//     console.log(i)
+//     i++;
+//     if(i === 10) flag = true;
+// }
