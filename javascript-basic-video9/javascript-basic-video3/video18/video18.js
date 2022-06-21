59502443d12d0e69e28275dd6cb6c1836fd61044
Khoa Huynh <huynhduykhoa03@gmail.com>
2022-06-21 14:09:40 +0700

    chapter 4 video 18

---
+++ javascript-basic-video9/javascript-basic-video3/video18/video18.js
@@ -0,0 +1,12 @@
+console.log('Hello world from HTML')
+
+// function sum(x, y){
+//     return x + y;
+// }
+
+//arrow function
+let sum = (x, y) => {
+    return x + y;
+}
+
+console.log('check sum: ', sum(6, 9))
