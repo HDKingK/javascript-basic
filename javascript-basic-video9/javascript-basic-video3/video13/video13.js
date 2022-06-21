7e38e310c7a2dfb13cb44e1e47ea0e6863e4fb81
Khoa Huynh <huynhduykhoa03@gmail.com>
2022-06-21 12:32:10 +0700

    chapter 3 video 15

--- javascript-basic-video9/javascript-basic-video3/video13/video13.js
+++ javascript-basic-video9/javascript-basic-video3/video13/video13.js
@@ -4,10 +4,10 @@ let arr = [`Liv`, `Chelsea`, `Mu`, `MC`]
 
 let i = 0;
 while (i < arr.length){
-    if (arr[i] === 'Chelsea') {
+    i++;
+    console.log('>>> check i: ', i)
+    if (arr[i] === 'Liv') {
         console.log('Found it: ', arr[i])
         break;
     }
-    console.log('>>> check i: ', i)
-    i++;
 }
