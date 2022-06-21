ca14d9cb39a2738faa162f360922a289c6e4b9bb
Khoa Huynh <huynhduykhoa03@gmail.com>
2022-06-21 08:54:02 +0700

    chapter 2 video10

---
+++ javascript-basic-video9/javascript-basic-video3/video10/video10.js
@@ -0,0 +1,17 @@
+console.log('Hello world from HTML')
+let a = 5; // x <- y
+let b = '5';
+
+console.log(' compare a == b: ', 'a = ', a, 'b = ', b, 'result: ', a == b)
+console.log(' compare a === b: ', 'a = ', a, 'b = ', b, 'result: ', a === b)
+
+// Empty, null, undefined
+let c = null;
+console.log('check value: ', c)
+let d = '';
+d = 5
+d = ['11']
+console.log(d)
+
+
+// === type, value convention
---
+++ javascript-basic-video9/javascript-basic-video3/video9/video9.js
@@ -0,0 +1,13 @@
+console.log('hello world')
+let a = {
+    name: 'Khoa',
+    age: 25
+}
+
+console.log('check data obj:', a.age)
+console.log('my name\'s:', a.name)
+
+//arr = [item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Liver'];
+console.log('type of b: ', typeof b, b)
+console.log('Tu day di len: ', b[0], ' Quy xanh: ', b[1], ' Mohamed Salah: ', b[2])
